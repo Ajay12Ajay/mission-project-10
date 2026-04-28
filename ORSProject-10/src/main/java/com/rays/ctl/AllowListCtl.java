800e890f7d2c84983f4ba29da005b049c2794daa
Ajay <ajaystl3@gmail.com>
2026-04-27 20:32:02 -0700

    done

--- ORSProject-10/src/main/java/com/rays/ctl/AllowListCtl.java
+++ ORSProject-10/src/main/java/com/rays/ctl/AllowListCtl.java
@@ -1,9 +1,14 @@
 package com.rays.ctl;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rays.common.BaseCtl;
+import com.rays.common.DropdownList;
+import com.rays.common.ORSResponse;
 import com.rays.dto.AllowListDTO;
 import com.rays.form.AllowListForm;
 import com.rays.service.AllowListServiceInt;
@@ -11,5 +16,21 @@ import com.rays.service.AllowListServiceInt;
 @RestController
 @RequestMapping(value = "AllowList")
 public class AllowListCtl extends BaseCtl<AllowListDTO, AllowListForm, AllowListServiceInt> {
+	
+	@GetMapping("/preload")
+	public ORSResponse preload() {
+		
+		AllowListDTO dto = new AllowListDTO();
+		
+		ORSResponse res=new ORSResponse(true);
+		
+		List<DropdownList> allowList=service.search(dto, userContext);
+		
+		res.addResult("allowList", allowList);
+		
+		return res;
+		
+		
+	}
 
 }
--- ORSProject-10/src/main/java/com/rays/service/AllowListServiceImpl.java
+++ ORSProject-10/src/main/java/com/rays/service/AllowListServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.rays.common.BaseServiceImpl;
+import com.rays.common.UserContext;
 import com.rays.dao.AllowListDAOInt;
 import com.rays.dto.AllowListDTO;
 
@@ -11,5 +12,14 @@ import com.rays.dto.AllowListDTO;
 @Transactional
 public class AllowListServiceImpl extends BaseServiceImpl<AllowListDTO, AllowListDAOInt>
 		implements AllowListServiceInt {
+
+	/*
+	 * @Transactional(readOnly = false)
+	 * 
+	 * @Override public long add(AllowListDTO dto, UserContext userContext) { long
+	 * pk = dao.add(dto, userContext); System.out.println("AllowList Record =====>"
+	 * + dao.findByPk(pk, userContext)); if (true) { throw new RuntimeException(); }
+	 * return pk; }
+	 */
 
 }
